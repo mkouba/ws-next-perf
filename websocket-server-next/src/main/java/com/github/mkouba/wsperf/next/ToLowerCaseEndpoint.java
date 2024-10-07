6720e405edca08c5d3903a6ad6c8531db205ce35
Martin Kouba <mkouba@redhat.com>
2024-10-07 11:43:01 +0200

    WS next - use the blocking signature in ToLowerCaseEndpoint by default

--- websocket-server-next/src/main/java/com/github/mkouba/wsperf/next/ToLowerCaseEndpoint.java
+++ websocket-server-next/src/main/java/com/github/mkouba/wsperf/next/ToLowerCaseEndpoint.java
@@ -3,12 +3,15 @@ package com.github.mkouba.wsperf.next;
 import io.quarkus.websockets.next.InboundProcessingMode;
 import io.quarkus.websockets.next.OnTextMessage;
 import io.quarkus.websockets.next.WebSocket;
+//import io.smallrye.mutiny.Uni;
 
 @WebSocket(path = "to-lower-case", inboundProcessingMode = InboundProcessingMode.CONCURRENT)
 public class ToLowerCaseEndpoint {
 
     @OnTextMessage
+    //Uni<String> convert(String message) {
     String convert(String message) {
+        //return Uni.createFrom().item(message.toLowerCase());
         return message.toLowerCase();
     }
 
