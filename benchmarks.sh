4c6f172304d89e5b994f3187040e9fa2b9557276
Martin Kouba <mkouba@redhat.com>
2024-11-05 14:12:36 +0100

    Script - generate flame graphs

--- benchmarks.sh
+++ benchmarks.sh
@@ -2,15 +2,45 @@
 if [ -z "$QUARKUS_VERSIONS" ]; then
     QUARKUS_VERSIONS="3.15.1 999-SNAPSHOT"
 fi
+
 if [ -z "$WARMUP_CLIENTS" ]; then
     WARMUP_CLIENTS=100
 fi
+
 if [ -z "$TEST_CLIENTS" ]; then
     TEST_CLIENTS=1000
 fi
+
 if [ -z "$TIMEOUT" ]; then
     TIMEOUT=60
 fi
+
+# Use "-flame" to generate flame graphs with async profiler
+# If async profiler is used then the following runtime variables must be set:
+# sysctl kernel.perf_event_paranoid=1
+# sysctl kernel.kptr_restrict=0
+if [ -z "$ASYNC_PROFILER_PATH" ]; then
+    ASYNC_PROFILER_PATH="/opt/java/async-profiler-3.0-linux-x64"
+fi
+
+contains_arg() {
+    local search="$1"
+    shift
+    local found=false
+
+    for arg in "$@"; do
+        if [[ "$arg" == "$search" ]]; then
+            found=true
+            break
+        fi
+    done
+
+    if [[ "$found" == true ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 wait_for_start() {
     counter=1
@@ -63,14 +93,34 @@ do
     cd ../test-client
     # run warmup phase
     java -Dnumber.of.clients=$WARMUP_CLIENTS -Dtimeout=$TIMEOUT -jar target/quarkus-app/quarkus-run.jar
+
+    # start async profiler if needed
+    if contains_arg "-flame" "$@"; then
+        # Get the server PID
+        PID="$(jps | grep quarkus-run | cut -d ' ' -f 1)"
+        $ASYNC_PROFILER_PATH/bin/asprof start $PID
+        echo "Async profiler started for server PID: $PID"
+    fi
+
     # run benchmark phase
     java -Dnumber.of.clients=$TEST_CLIENTS -Dtimeout=$TIMEOUT -jar target/quarkus-app/quarkus-run.jar
+
+    # go to project root
+    cd ../
+
+    # stop async profiler if needed
+    if contains_arg "-flame" "$@"; then
+        # Get the server PID
+        PID="$(jps | grep quarkus-run | cut -d ' ' -f 1)"
+        FILE="flame_$i.html"
+        $ASYNC_PROFILER_PATH/bin/asprof stop -f $FILE -o flamegraph $PID
+        echo "Async profiler stopped for server PID: $PID, output file: $FILE"
+    fi
 
     # wait for the server to stop
     wait_for_stop
 
     echo "Testing $i finished..."
-    cd ../
 done
 
 cd test-client
